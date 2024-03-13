b01e883bdbbc1dc1b03e8bfcccd19057cafd5f6a
Luiz Henrique Carneiro Carvalho <luizhenriqueccarvalho@gmail.com>
2024-03-13 17:20:17 -0300

    arrumando valor

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,28 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Econol',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const EconomolPage(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class EconomolPage extends StatefulWidget {
+  const EconomolPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _EconomolPageState createState() => _EconomolPageState();
+}
+
+class _EconomolPageState extends State<EconomolPage> {
   final TextEditingController _gasolinaController = TextEditingController();
   final TextEditingController _etanolController = TextEditingController();
   String _resultado = '';
@@ -23,6 +39,7 @@ class _MyAppState extends State<MyApp> {
     num gasolina = numberFormat.parse(gasolinaText);
     num etanol = numberFormat.parse(etanolText);
 
+    // ignore: unnecessary_null_comparison
     if (etanol != null) {
       double proporcao = etanol / gasolina;
       if (proporcao <= 0.7) {
@@ -40,52 +57,59 @@ class _MyAppState extends State<MyApp> {
   void _formatarNumero(TextEditingController controller) {
     String text = controller.text;
     if (text.isNotEmpty) {
-      NumberFormat numberFormat = NumberFormat('0.00', 'pt_BR');
-      String formattedText = numberFormat.format(double.parse(text));
-      if (formattedText != text) {
-        controller.value = controller.value.copyWith(
-          text: formattedText,
-          selection: TextSelection.collapsed(offset: formattedText.length),
-        );
-      }
+      // Remove vírgulas e pontos
+      text = text.replaceAll(',', '').replaceAll('.', '');
+
+      // Converter para número
+      num value = num.tryParse(text) ?? 0;
+
+      // Formatar para exibição
+      NumberFormat numberFormat =
+          NumberFormat.currency(locale: 'pt_BR', symbol: '', decimalDigits: 2);
+      String formattedText = numberFormat.format(value / 100);
+
+      // Atualizar o texto no controlador
+      controller.value = controller.value.copyWith(
+        text: formattedText,
+        selection: TextSelection.collapsed(offset: formattedText.length),
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Comparador de Combustível'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _gasolinaController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Preço da gasolina'),
-                onEditingComplete: () => _formatarNumero(_gasolinaController),
-              ),
-              TextField(
-                controller: _etanolController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: 'Preço do etanol'),
-                onEditingComplete: () => _formatarNumero(_etanolController),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _calcularMelhorOpcao,
-                child: const Text('Calcular'),
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                _resultado,
-                style: const TextStyle(fontSize: 18.0),
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Econol'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _gasolinaController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Preço da gasolina'),
+              onChanged: (_) => _formatarNumero(_gasolinaController),
+            ),
+            TextField(
+              controller: _etanolController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(labelText: 'Preço do etanol'),
+              onChanged: (_) => _formatarNumero(_etanolController),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _calcularMelhorOpcao,
+              child: const Text('Calcular'),
+            ),
+            const SizedBox(height: 16.0),
+            Text(
+              _resultado,
+              style: const TextStyle(fontSize: 18.0),
+            ),
+          ],
         ),
       ),
     );
