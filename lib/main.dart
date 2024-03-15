121dfa1d78365cefefe0eac74a2c3988e6816c12
Luiz Henrique Carneiro Carvalho <luizhenriqueccarvalho@gmail.com>
2024-03-15 17:33:06 -0300

    primeira versão estável

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Econol',
+      title: 'ECONOL',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
--- lib/templates/home.dart
+++ lib/templates/home.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:econol/core/calculo.dart';
 import 'package:econol/core/button_animation.dart';
 
+
 class EconomolPage extends StatefulWidget {
   const EconomolPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _EconomolPageState createState() => _EconomolPageState();
 }
 
@@ -13,6 +15,20 @@ class _EconomolPageState extends State<EconomolPage> {
   final TextEditingController _gasolinaController = TextEditingController();
   final TextEditingController _etanolController = TextEditingController();
   String _resultado = '';
+  bool _showResult = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _gasolinaController.addListener(_updateResultVisibility);
+    _etanolController.addListener(_updateResultVisibility);
+  }
+
+  void _updateResultVisibility() {
+    setState(() {
+      _showResult = false;
+    });
+  }
 
   void _calcularMelhorOpcao() {
     String gasolinaText = _gasolinaController.text.replaceAll(',', '.');
@@ -26,18 +42,21 @@ class _EconomolPageState extends State<EconomolPage> {
     num gasolina = parseNumber(gasolinaText);
     num etanol = parseNumber(etanolText);
 
+    // ignore: unnecessary_null_comparison
     if (etanol != null) {
       double proporcao = etanol / gasolina;
-      if (proporcao <= 0.7) {
-        _resultado = 'Abasteça com etanol.';
-      } else {
-        _resultado = 'Abasteça com gasolina.';
-      }
+      setState(() {
+        _resultado = proporcao <= 0.7
+            ? 'Abasteça com etanol.'
+            : 'Abasteça com gasolina.';
+        _showResult = true;
+      });
     } else {
-      _resultado = 'Digite valores válidos.';
+      setState(() {
+        _resultado = 'Digite valores válidos.';
+        _showResult = true;
+      });
     }
-
-    setState(() {});
   }
 
   void _formatarNumero(TextEditingController controller) {
@@ -67,53 +86,84 @@ class _EconomolPageState extends State<EconomolPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Econol'),
+        title: const Text('ECONOL'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            const SizedBox(height: 50.0),
             TextField(
               controller: _gasolinaController,
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'Preço da gasolina'),
+              decoration: const InputDecoration(
+                labelText: 'Preço da Gasolina',
+                prefixIcon: Icon(Icons.local_gas_station),
+              ),
               onChanged: (_) => _formatarNumero(_gasolinaController),
             ),
+            const SizedBox(height: 40.0),
             TextField(
               controller: _etanolController,
               keyboardType: TextInputType.number,
-              decoration: const InputDecoration(labelText: 'Preço do etanol'),
+              decoration: const InputDecoration(
+                labelText: 'Preço do Etanol',
+                prefixIcon: Icon(Icons.local_drink),
+              ),
               onChanged: (_) => _formatarNumero(_etanolController),
             ),
-            const SizedBox(height: 40.0),
+            const SizedBox(height: 60.0),
             Center(
               child: SizedBox(
                 width: 200,
                 child: PushableButton(
                   height: 60,
                   elevation: 8,
-                  hslColor: const HSLColor.fromAHSL(1.0, 120, 1.0, 0.37),
+                  hslColor: const HSLColor.fromAHSL(1.0, 0, 0, 0.5),
                   shadow: const BoxShadow(
                     color: Colors.grey,
                     spreadRadius: 2,
                     blurRadius: 7,
                     offset: Offset(0, 2),
                   ),
-                  onPressed: _calcularMelhorOpcao,
-                  width: 40,
-                  child: const Text('Calcular', style: TextStyle(fontSize: 30)),
+                  onPressed: () {
+                    _calcularMelhorOpcao();
+                    if (!_showResult) _scrollToResult();
+                  },
+                  width: 20,
+                  child: const Text(
+                    'Calcular',
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
             ),
-            const SizedBox(height: 16.0),
-            Text(
-              _resultado,
-              style: const TextStyle(fontSize: 18.0),
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut,
+              height: _showResult ? 50 : 0, // Altura da animação
+              child: Center(
+                child: Text(
+                  _resultado,
+                  style: const TextStyle(fontSize: 18.0),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _scrollToResult() {
+    Future.delayed(const Duration(milliseconds: 500), () {
+      Scrollable.ensureVisible(context,
+          alignment: 0.5, duration: const Duration(milliseconds: 500));
+    });
   }
 }
